72f89fc6d52e2888152579da6de8246c274f898a
Antony Konstantinidis <ak@digitalessence.de>
2021-06-11 11:41:33 +0200

    add logic

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <header v-if="!hasSearchResult">
+    <h1>Movie Finder</h1>
+    <p>Search your movie</p>
+    <input
+      type="text"
+      placeholder="Title"
+      v-model="title"
+      @keypress.enter="onClick"
+    />
+    <input
+      type="text"
+      placeholder="Year"
+      v-model="year"
+      @keypress.enter="onClick"
+    />
+    <button @click="onClick">Search</button>
+  </header>
+  <Result v-bind="resultNormalized" v-else @reset="onReset" />
+  <!-- <Result
+  :title="result.Title"
+  :year="result.Year"
+  :actors="result.Actors"
+  :genre="result.Genre"
+  :production="result.Production"
+  :director="result.Director"
+  :country="result.Country"
+  :awards="result.Awards"
+  :plot="result.Plot"
+  @reset="onReset" /> -->
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Result from "./components/Result.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Result,
+  },
+  data() {
+    return {
+      title: "",
+      year: "",
+      result: {},
+      hasSearchResult: false,
+    };
+  },
+  computed: {
+    resultNormalized() {
+      let entries = Object.entries(this.result);
+      entries = entries.map((entry) => {
+        entry[0] = entry[0].toLowerCase();
+        return entry;
+      });
+      return Object.fromEntries(entries);
+    },
+  },
+  methods: {
+    onClick() {
+      if (this.title !== "") {
+        fetch(
+          "https://www.omdbapi.com/?apikey=167eb644&t=" +
+            this.title +
+            "&y=" +
+            this.year
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            this.result = data;
+            this.hasSearchResult = true;
+          });
+      }
+    },
+    onReset() {
+      this.result = {};
+      this.hasSearchResult = false;
+      this.title = "";
+      this.year = "";
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+header {
+  background-image: url("https://www.decopoint.at/media/image/product/94792/md/filmstreifen-kunststoff-140x19cm-art_nr7344641.jpg");
+  background-size: cover;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: 400px;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h2>Result</h2>
+
+    <p>{{ title }}</p>
+
+    <p>{{ year }}</p>
+
+    <p>{{ actors }}</p>
+
+    <p>{{ genre }}</p>
+
+    <p>{{ production }}</p>
+
+    <p>{{ director }}</p>
+
+    <p>{{ country }}</p>
+
+    <p>{{ awards }}</p>
+
+    <p>{{ plot }}</p>
+  </div>
+  <button @click="goBack">Zurück zur Suche</button>
+</template>
+
+<script>
+export default {
+  name: "Result",
+  props: {
+    title: String,
+    year: String,
+    actors: String,
+    genre: String,
+    production: String,
+    director: String,
+    country: String,
+    awards: String,
+    plot: String,
+  },
+  methods: {
+    goBack() {
+      this.$emit("reset");
+    },
+  },
+  emits: ["reset"],
+};
+</script>
